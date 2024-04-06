4b466d47631719bc6d755d36679214427ee556d2
Neil Gall <neil@gall.scot>
2024-04-06 21:51:22 +0100

    Add docker-start script

---
+++ docker-start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source .env
+
+docker stop ev-automation
+docker rm ev-automation
+
+docker build -t ev-automation .
+
+docker run \
+    --name ev-automation \
+    --restart always \
+    --detach \
+    -e ANDERSEN_USERNAME="$ANDERSEN_USERNAME" \
+    -e ANDERSEN_PASSWORD="$ANDERSEN_PASSWORD" \
+    -e ANDERSEN_DEVICE_NAME="$ANDERSEN_DEVICE_NAME" \
+    -e RENAULT_USERNAME="$RENAULT_USERNAME" \
+    -e RENAULT_PASSWORD="$RENAULT_PASSWORD" \
+    -e RENAULT_REGISTRATION="$RENAULT_REGISTRATION" \
+    -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+    -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+    -e AWS_DEFAULT_REGION=eu-west-1 \
+    ev-automation
